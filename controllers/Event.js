86e34f9c0e1d9db99c761a45a83fbf3ac757fa8a
rpigozzi <riccardo.pigozzi@mail.polimi.it>
2019-04-30 19:42:53 +0200

    Made changes to Event and EventService

--- controllers/Event.js
+++ controllers/Event.js
@@ -7,19 +7,19 @@ module.exports.getEventByID = function getEventByID (req, res, next) {
   var eventID = req.swagger.params['eventID'].value;
   Event.getEventByID(eventID)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response.actualResponse, response.status);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response.actualResponse, response.status);
     });
 };
 
 module.exports.getEvents = function getEvents (req, res, next) {
   Event.getEvents()
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response.actualResponse, response.status);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response.actualResponse, response.status);
     });
 };
--- service/EventService.js
+++ service/EventService.js
@@ -1,60 +1,40 @@
-'use strict';
+'use strict'
 
+const knex = require('../knex.js');
+const db = knex.database;
 
 /**
  * Returns a single event by ID
  *
- * eventID Long ID of the event to return
+ * eventID: ID of the wanted event
  * returns Event
- **/
+ */
 exports.getEventByID = function(eventID) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "book" : {
-    "name" : "Il sentiero",
-    "id" : 0,
-    "authors" : [ {
-      "firstName" : "firstName",
-      "lastName" : "",
-      "id" : 0
-    }, {
-      "firstName" : "firstName",
-      "lastName" : "",
-      "id" : 0
-    } ]
-  },
-  "name" : "name"
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
+  return new Promise(async function(resolve, reject){
+    const event = await db.select().from('Event').where('eventID', eventID);
+    if (event.length <= 0){
+      reject({actualResponse: 'This event does not exist!', status: 404});
     }
-  });
+    else {
+      const books = await db.select('Book.bookID', 'name').from('Book').join('Event', {'Book.bookID' : 'Event.bookID'}).where('eventID', eventID);
+      resolve({actualResponse: event.concat(books), status: 200});
+    }
+  })
 }
-
 
 /**
  * Returns all events' essential data
- *
  * returns List
- **/
+ */
 exports.getEvents = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "name" : "name",
-  "id" : 0
-}, {
-  "name" : "name",
-  "id" : 0
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
+  return new Promise(async function (resolve, reject) {
+    const events = await db.select().from('Event');
+    if (events.length <= 0){
+      reject({actualResponse: 'No events planned', status: 404});
     }
-  });
+    else{
+      resolve({actualResponse: events, status: 200});
+    }
+  })
 }
 
