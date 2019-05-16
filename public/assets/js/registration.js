3024e2d6bc672a1ee8c5c3b25e959b1788b9def4
Cristiano Desoli <48205233+flycri@users.noreply.github.com>
2019-05-16 15:53:07 +0200

    Fix registration script

--- public/assets/js/registration.js
+++ public/assets/js/registration.js
@@ -1,31 +1,31 @@
 $(function (){
-    
+
     var $username = $('#registerUsername');
     var $firstName = $('#registerFirstname');
     var $lastName = $('#registerLastname');
     var $password = $('#registerPassword');
     var $email = $('#registerEmail');
     var $phone = $('#registerPhoneNumber');
-  
-$('#submitButton').click(function(){
-    
-    var submitData = {
-        username: $username.val(),
-        firstName: $firstName.val(),
-        lastName: $lastName.val(),
-        email: $email.val(),
-        password: $password.val(),
-        phone: $phone.val(),
-    };
+
+    $('#submitButton').click(function(){
+
+        var formArray = $('#registrationForm').serializeArray();
+
+        var returnArray = {};
+        for (var i = 0; i < formArray.length; i++){
+            returnArray[formArray[i]['name']] = formArray[i]['value'];
+        }
+
+        console.log(JSON.stringify(returnArray));
 
 
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8080/xXEmilioXx/MyBookstore/1.0.0/user/register",
-        data: JSON.stringify($('#registrationForm').serialize()),
-        contentType: "application/json;",
-        dataType: "json",
+        $.ajax({
+            type: "POST",
+            url: "/xXEmilioXx/MyBookstore/1.0.0/user/register",
+            data: JSON.stringify(returnArray),
+            contentType: "application/json",
+            dataType: "json",
+        });
+
     });
-    
-});
 })
