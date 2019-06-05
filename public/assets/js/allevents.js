2e9175ac7cba0802f38bfd2085268d204b828672
Cristiano Desoli <48205233+Flycri@users.noreply.github.com>
2019-06-05 03:23:43 +0200

    Add allevents, best_sellers and book html and js files (best sellers.js TODO)

---
+++ public/assets/js/allevents.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+
+    $.ajax({
+        type : 'GET',
+        url : '/xXEmilioXx/MyBookstore/1.0.0/event',
+        dataType : 'json',
+        success : function(response){
+            createPages(response);
+        }
+    });
+
+    function createPages(response){
+        var i = 0;
+        var j = 0;
+        var pageSize = 5;
+        var obj = JSON.stringify(response);
+        var jsArray = JSON.parse(obj);
+        var arrayLength = jsArray.length;
+        var pageCount =  arrayLength / pageSize;
+
+        for( i; i < arrayLength; i++){
+            $(".card-columns").append('<div class="card"><a href="author.html?parameter=' + jsArray[i].authorID + '" class="card-link"><img class="card-img" src="' + jsArray[i].image_path + '" alt="Card img cap"><div class="card-body"><h5 class="card-title">' + jsArray[i].name + " - " + jsArray[i].date + " - " + jsArray[i].time + '</h5></div></a></div>');
+        }
+
+        if (pageCount > 1){
+            $(".pagination").append('<li id="first" class="page-item"><a href="#" aria-label="First"><span aria-hidden="true">&laquo;</span></a></li>');
+        }
+
+        for( j ; j < pageCount; j++){
+            $(".pagination").append('<li class="page-item"><a href="#">'+(j+1)+'</a></li>');
+        }
+
+        if (pageCount > 1){
+            $(".pagination").append('<li id="last" class="page-item"><a href="#" aria-label="Last"><span aria-hidden="true">&raquo;</span></a></li>');
+        }
+
+        if (pageCount === 1){
+            $(".pagination li").first().find("a").addClass("current");
+        }
+
+        else {
+            $(".pagination li").first().next().find("a").addClass("current");
+        }
+
+        showPage = function(page) {
+            $(".card").hide();
+            $(".card").each(function(n) {
+                if (n >= pageSize * (page - 1) && n < pageSize * page)
+                    $(this).show();
+            });
+        };
+
+        showPage(1);
+
+        $("#first").click(function() {
+            $(".pagination li a").removeClass("current");
+            $("#first").next().find("a").addClass("current");
+            showPage(parseInt($("#first").next().find("a").text()))
+        });
+
+        $("#last").click(function() {
+            $(".pagination li a").removeClass("current");
+            $("#last").prev().find("a").addClass("current");
+            showPage(parseInt($("#last").prev().find("a").text()))
+        });
+
+        $(".pagination li a").click(function() {
+            $(".pagination li a").removeClass("current");
+            $(this).addClass("current");
+            showPage(parseInt($(this).text()))
+        });
+    }
+});
---
+++ public/assets/js/book.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/xXEmilioXx/MyBookstore/1.0.0/book/' + getURLQueryParameter(),
+        datatype: 'json',
+        success: function (response) {
+            adjustBookPage(response);
+        }
+    });
+
+    function createGenresList(genresArray) {
+        var i = 0;
+        var nGenres = genresArray.length;
+
+        for (i; i < nGenres; i++) {
+            $("#genreList").append('<a href="allbookgenres.html?genre=' + genresArray[i] + '</a>')
+        }
+    }
+
+    function createThemesList(themesArray) {
+        var i = 0;
+        var nThemes = themesArray.length;
+
+        for (i; i < nThemes; i++) {
+            $("#themeList").append('<a href="allbookthemes.html?parameter=' + themesArray[i] + '</a>')
+        }
+    }
+
+    function createSimilarBookList(similarBooksArray) {
+        var i = 0;
+        var nSimilarBooks = similarBooksArray.length;
+
+        for (i; i < nSimilarBooks; i++) {
+            $("#similarBooksList").append('<a href="book.html?parameter=' + similarBooksArray[i].bookID + '" class="list-group-item list-group-item-action">' + similarBooksArray[i].name + '</a>')
+        }
+    }
+
+    function createAuthorList(authorArray) {
+        var i = 0;
+        var nAuthors = authorArray.length;
+
+        for (i; i < nAuthors; i++) {
+            $("#authorList").append('<a href="author.html?parameter=' + authorArray[i].authorID + '" class="list-group-item list-group-item-action">' + authorArray[i].name + '</a>')
+        }
+    }
+
+
+    function adjustBookPage(bookjson) {
+        var book = JSON.parse(JSON.stringify(bookjson));
+        $(".card-img").attr("src", book[0].image_path);
+        document.getElementById("id").innerHTML = "Book" + " " + "#" + book[0].bookID;
+        document.getElementById("name").innerHTML = book[0].name;
+        document.getElementById("edition").innerHTML =
+            book[0].edition;
+        document.getElementById("cost").innerHTML =
+            book[0].cost;
+        createAuthorList(book[0].authors);
+        createGenresList(book[0].genres);
+        createThemesList(book[0].themes);
+        createSimilarBookList(book[0].similarBooks);
+    }
+
+})
