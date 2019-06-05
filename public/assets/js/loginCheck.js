059f5cee57228a4f6f9b997bfba0dcb8f4875378
rpigozzi <riccardo.pigozzi@mail.polimi.it>
2019-06-05 22:04:03 +0200

    Mostly made changes to max-width and max-height of various elements

--- public/assets/js/loginCheck.js
+++ public/assets/js/loginCheck.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
         loginItem.append('<h5> You are logged in </h5>' +
             '<a href="#" type="submit" class="btn btn-primary mb-2" id="submitLogoutButton">Log out</a>' +
             '<a href="../../pages/profile.html" type="submit" class="btn btn-primary mb-2" id="profileButton">My profile</a>' +
-            '<a href="shoppingcart.html">\n' +
+            '<a href="../../pages/shoppingcart.html">\n' +
             '<img src="../assets/img/baseline-shopping_cart-24px.svg" alt="cart">\n' +
             '</a>');
 
