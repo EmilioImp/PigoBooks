b664b3fb7a9f7a9c33a6851c773262e1bcd45954
Cristiano Desoli <48205233+flycri@users.noreply.github.com>
2019-06-23 14:25:47 +0200

    add alternative img, fix minor style problems in all pages' header and footer, implement js for bestsellers (yet to test) and modify best sellers html page to fit js behaviour

--- public/assets/js/best_sellers.js
+++ public/assets/js/best_sellers.js
@@ -5,17 +5,39 @@ $(document).ready(function() {
         url: '/xXEmilioXx/MyBookstore/1.0.0/book/bestSellers',
         datatype: 'json',
         success: function (response) {
-            adjustBestSellersPage(response);
+            getFirstThreeBooks(response);
         }
     });
 
-    function getFirstThreeBooks(booksarray){
+    function generateAuthorLinks(authorlist) {
+        var j = 0;
+        var string = "";
+        for (j; j < authorlist[j].length - 1; j++) {
+            string = string + '<a href="author.html?parameter=' + authorlist[j].authorID + ', ' + '>' + authorlist[j].firstName + ' ' + authorlist[j].lastName + '</a>';
+        }
+        string = string + '<a href="author.html?parameter=' + authorlist[j].authorID + '>';
+        return string;
+    }
 
+    function getFirstThreeBooks(response){
+        var i = 0;
+        const maxBooks=3
+        var obj = JSON.stringify(response);
+        var jsArray = JSON.parse(obj); //converting the json array in js objects array
+        for (i; i<maxBooks-1;i++){
+            $(".top-books").append('<div class="row featurette" id="top-book">\n' +
+                '         <div class="col-md-7 order-md-2">\n' +
+                '           <h2 class="featurette-heading"><span class="badge badge-secondary" id="position">' + jsArray[i].placement + '</span></h2>\n' +
+                '           <p class="lead" id="authors">'+ generateAuthorLinks(jsArray[i].authors)
+                +'</p>\n' +
+                '         </div>\n' +
+                '         <div class="col-md-5 order md-1">\n' +
+                '           <img class="img-fluid" id="book-img" src="'+ jsArray[i].image_path +'" alt="book image">\n' +
+                '         </div>\n' +
+                '       </div>\n' +
+                '         <hr class="featurette-divider">')
+        }
     }
 
 
-
-    function adjustBestSellersPage(response) {
-
-    }
 })
